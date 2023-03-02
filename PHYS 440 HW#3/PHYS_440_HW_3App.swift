0155d18b4b65d8efefe478c7eff15346306c0198
Clairezy <clairezwicker98@gmail.com>
2023-03-02 14:18:12 -0600

    Initial Commit

---
+++ PHYS 440 HW#3/PHYS_440_HW_3App.swift
@@ -0,0 +1,17 @@
+//
+//  PHYS_440_HW_3App.swift
+//  PHYS 440 HW#3
+//
+//  Created by Claire on 3/2/23.
+//
+
+import SwiftUI
+
+@main
+struct PHYS_440_HW_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PHYS 440 HW#3UITests/PHYS_440_HW_3UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PHYS_440_HW_3UITestsLaunchTests.swift
+//  PHYS 440 HW#3UITests
+//
+//  Created by Claire on 3/2/23.
+//
+
+import XCTest
+
+final class PHYS_440_HW_3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
